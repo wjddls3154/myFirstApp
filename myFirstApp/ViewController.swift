5addc9661404c650d1e8f24073c64db40992b286
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-03-12 15:21:35 +0900

    add background color in viewController

--- myFirstApp/ViewController.swift
+++ myFirstApp/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("git commit test")
+        view.backgroundColor = UIColor.yellow
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
